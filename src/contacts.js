ad312cf6fc3dbf79722dfa667b8595343b6fd09d
Denis <k1selevde@yandex.ru>
2022-05-01 15:01:04 +0500

    task perfomance

--- src/contacts.js
+++ src/contacts.js
@@ -1,6 +1,9 @@
 const contacts = document.getElementsByClassName("contacts")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
 
+var items;
+var itemOffsets;
+
 function addContacts() {
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < 50000; i++) {
@@ -10,17 +13,15 @@ function addContacts() {
     fragment.appendChild(child);
   }
   contacts.appendChild(fragment);
+
+  items = Array.from(contacts.getElementsByClassName("contact"));
+  itemOffsets = items.map((item) => item.offsetTop);
 }
 
-contacts.addEventListener("scroll", (e) => {
-  const items = Array.from(contacts.getElementsByClassName("contact"));
-  const itemOffsets = items.map((item) => item.offsetTop);
-  const topItemIndex = itemOffsets.findIndex(
-    (offset) => contacts.scrollTop - offset <= -18
-  );
-  if (topItemIndex !== -1) {
-    stickyHeader.textContent = items[topItemIndex].textContent;
-  }
+
+
+contacts.addEventListener("scroll", () => {
+  stickyHeader.textContent = Math.floor(contacts.scrollTop / 18);
 });
 
 addContacts();
